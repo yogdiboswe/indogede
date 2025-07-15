6b02e86fbba56478ab5d85c3a54cb1d4fe1f4a22
yogdiboswe <yogdiboswe@gmail.com>
2025-07-15 16:08:11 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Indo Gede';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Gede Montok Smp Mama Live Viral Colmek Sma Hijab Tante China';
-export const url = 'https://indogede.pages.dev';
-export const terbit = '2025-01-26T00:01:03Z';
