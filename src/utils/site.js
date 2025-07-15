26c1be22b52b0dc9bcf9b12cf6df660a58539c34
yogdiboswe <yogdiboswe@gmail.com>
2025-07-15 16:09:11 +0700

    Add files via upload

---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const nama = 'Indo Gede';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Gede Montok Smp Mama Live Viral Colmek Sma Hijab Tante China';
+export const url = 'https://indogede.pages.dev';
+export const terbit = '2025-01-26T00:01:03Z';
+
+export const google = 'Mc_ZlvlZosbroaLcSGi5WVc7C0eUp6g2e3juLvFY_5o';
+export const yandex = 'dee9fd51e42c90c2'
+
+export const index = '49dd38ae-c515-45ca-9ffb-1ccfdd730b93';
